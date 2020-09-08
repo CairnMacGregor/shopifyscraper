723f11cdffbc1e4d1f58d8b50be3c3423afa6f7a
Cairn MacGregor <cmacg2@hotmail.co.uk>
2020-09-08 12:00:37 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Scraper msg="Use this web app to Scrape products from existing Shopify stores"/>
+  </div>
+</template>
+
+<script>
+import Scraper from './components/Scraper.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Scraper,
+  }
+}
+</script>
+
+<style>
+html {
+  box-sizing: border-box;
+  font-size: 16px;
+  background: grey;
+}
+
+*, *:before, *:after {
+  box-sizing: inherit;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
---
+++ src/components/Scraper.vue
@@ -0,0 +1,111 @@
+<template>
+<section>
+
+   <form class = "form-inline" v-on:submit.prevent="onSubmit">
+         <div class="form-group mb-2">
+             <label for="">Enter the URL </label>
+             <input class="form-control form-control-md" 
+                type="text"
+                v-model="url" 
+                id="url">
+        </div>
+         <div class="form-group mb-2">
+                <input type="submit" value ="Submit" class = "btn btn-primary">
+            </div>
+          
+      </form>
+
+      <div class = "downloadbtn" v-if="ScrapeData">
+          <button class = "btn btn-primary form-group mb-2 ml-2" v-on:click="download">
+        
+            {{JsonData.data.products[0].title}}
+          <vue-json-to-csv v-if="ScrapeData"
+          :json-data = [{title:[JsonData.data.products[0].title],id:[JsonData.data.products[0].id]}]
+          :csv-title =" 'Shopify Scrape Data' + `${this.url}` ">
+          </vue-json-to-csv></button>
+          {{json-data}}
+      </div>
+
+ 
+
+      <div class="ProductContainer" v-if="ScrapeData">
+      <div v-for="(value, index) in ScrapeData.data.products"  
+        v-bind:key="index">
+            <div v-if="value.variants[0].sku" class = "card">
+              <img class = "card-img-top" :src="value.images[0].src" alt="">
+              <div class="card-body">
+               
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item"><p class = "card-text">{{value.title}}</p> </li>
+                  <li class="list-group-item bg-success"><p class = "card-text">SKU = {{value.variants[0].sku}}</p>  </li>
+                  <li class="list-group-item"><p class = "card-text">{{value.body_html}}</p></li>
+                </ul>
+              </div>
+            </div>
+            <div v-else class = "card">
+              <img class = "card-img-top" :src="value.images[0].src" alt="">
+              <div class="card-body">
+                <ul class="list-group list-group-flush ">
+                  <li class="list-group-item"><p class = "card-text">{{value.title}}</p> </li>
+                  <li class="list-group-item card text-light bg-danger"><p class = "card-text">SKU = Product does not have an SKU 😔</p>  </li>
+                  <li class="list-group-item"><p  class = "card-text">{{value.body_html}}</p></li>
+                </ul>
+              </div>
+            </div>
+      </div>
+      </div>
+
+</section>
+</template>
+
+
+<script>
+import axios from 'axios'
+import VueJsonToCsv from 'vue-json-to-csv'
+export default {
+  data(){
+    return{
+      ScrapeData: "",
+      JsonData: []
+    }
+  },
+  components:{
+    VueJsonToCsv
+  },
+
+methods:{
+ async onSubmit(){
+   const res = await axios.get(`${this.url}` + "/products.json")
+   this.ScrapeData = res,
+   this.JsonData = res  
+   },
+  download(){
+    console.log("clicked")
+  },
+  
+},
+created(){
+},
+}
+</script>
+
+<style>
+form
+{
+  display: flex;
+  justify-content: center;
+  background: grey;
+  padding: 5rem;
+}
+
+.downloadbtn{
+  background: grey;
+}
+
+.ProductContainer{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+    grid-gap: 10px;
+    background: grey;
+ }
+</style>
